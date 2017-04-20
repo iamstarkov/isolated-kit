3bed8427c2b26d36a3362ca2434593a68885445f
Vladimir Starkov <iamstarkov@gmail.com>
2017-04-20 02:42:41 +0200

    feat: expose new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Alert from './alert';
 import Button from './button';
+import DynamicThemedBox from './dynamic-themed-box';
+import StaticThemedBox from './static-themed-box';
 
 export {
   Alert,
   Button,
+  DynamicThemedBox,
+  StaticThemedBox,
 }
